159fc6a6634657ad7d779a045561a37c399c7aea
Tre Ammatuna <tammatuna@avvo.com>
2018-11-25 23:54:10 -0800

    Foundations solutions

--- 1-foundations-of-programming/1-foundations-solutions.js
+++ 1-foundations-of-programming/1-foundations-solutions.js
@@ -11,60 +11,71 @@
 
 // 1. Enter the following expressions into your console.
 
-1 + 2;
-3 * 5;
-5 / 4 - 13;
-5000 * 234;
-1073 / 57 + 200;
+1 + 2; // 3
+3 * 5; // 15
+5 / 4 - 13; // -11.75
+5000 * 234; // 1170000
+1073 / 57 + 200; // 218.82456140350877
 
 // 2. Why are the values produced by the following two expressions different?
 //    What are they?
 
-3 + 2 * 4 - 1;
-(3 + 2) * (4 - 1);
+3 + 2 * 4 - 1; // 10
+(3 + 2) * (4 - 1); // 15
 
 // 3. Calculate how old you are in minutes using the console.
+36 * 365 * 24 * 60; // 18921600
 
 // 4. What is the percentage of people in the class with brown hair?
 //    Use the console to find out.
 
 // 5. Try the following expressions in the console:
 
-6 % 2;
-42 % 10;
-5 % 2;
-6 % 3;
-7 % 4;
-100 % 12;
+6 % 2; // 0
+42 % 10; // 2
+5 % 2; // 1
+6 % 3; // 0
+7 % 4; // 3
+100 % 12; // 4
 
 // What is the significance of the result ? How does the`%`(modulus) operator work?
 
 // 6. Try the following:
 
-3 % 2;
-4 % 2;
-5 % 2;
-6 % 2;
+3 % 2; // 1
+4 % 2; // 0
+5 % 2; // 1
+6 % 2; // 0
 
 // What do the results tell you about the first operand to the modulus operator?
 
 // Strings
 
 // 1. Write a string that represents your full name.
+"Tre Ammatuna";
 
 // 2. Write a string that represents your favorite food.
+"Bluefin Tuna";
 
 // 3. Use the`+` operator to combine(known as *concatenation *) two or more strings, * e.g.*:
+"Tre Ammatuna" + " " + "loves bluefin tuna.";
 
 // Your first and last names
 "John" + " " + "Doe";
 
 // Your first and last names(as shown above)
+"Tre" + " " + "Ammatuna";
 // Your parents' full names
+"Jack" + " " + "Ammatuna";
+"Jill" + " " + "Ammatuna";
 // Your home town and state
+"Augusta" + ", " + "GA"
 
 // 4. Fix the errors in the following strings:
 
 // Where are all the quotes?
+"Where are all the quotes?";
 // 'hmm something is not right"
+"hmm something is not right";
 // 'Do other ' * 'operators work with string concatenation?'
+"No";
